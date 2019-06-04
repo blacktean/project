85e6e2e0a8834f5c0a365ae86d6b6b4e12fff96e
hcf <1429187588@qq.com>
2019-06-04 09:49:45 +0800

    上传by hcf

--- bankTransfer/bankTransfer-front/src/main/java/com/bankTransfer/BankTransferFrontApplication.java
+++ bankTransfer/bankTransfer-front/src/main/java/com/bankTransfer/BankTransferFrontApplication.java
@@ -4,7 +4,6 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
 @MapperScan("com.bankTransfer.mapper")
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class BankTransferFrontApplication {
