5cb7584208123ab467bfc8f59e4e89d1b947fa08
kkayy <3182971572@qq.com>
2019-06-04 09:52:08 +0800

    上传登陆 by 康   -6-4

---
+++ bankTransfer/bankTransfer-core/src/main/java/com/bankTransfer/util/LogininfoException.java
@@ -0,0 +1,27 @@
+package com.bankTransfer.util;
+
+public class LogininfoException extends Exception{
+
+	private static final long serialVersionUID = 1L;
+
+	public LogininfoException() {
+		super();
+	}
+
+	public LogininfoException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+	public LogininfoException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public LogininfoException(String message) {
+		super(message);
+	}
+
+	public LogininfoException(Throwable cause) {
+		super(cause);
+	}
+
+}
