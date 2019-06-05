4abd302d8c372d819ccd512d63a0bdc44c53d5a4
kkayy <3182971572@qq.com>
2019-06-05 16:30:05 +0800

    上传了注册 by 康 6-5

---
+++ bankTransfer/bankTransfer-core/src/main/java/com/bankTransfer/mapper/RegisterMapper.java
@@ -0,0 +1,33 @@
+package com.bankTransfer.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import org.apache.ibatis.annotations.Param;
+
+import com.bankTransfer.pojo.Logininfo;
+/***
+ * 注册
+ * @author 康志龙
+ *@version  创建时间   2019年6月5日  下午3:17:59
+ */
+@Mapper
+public interface RegisterMapper {
+/**
+ * 检验用户名是否已经被注册
+ * @param username 用户名
+ * @return
+ */
+	Logininfo checkUsername(@Param("username")String username);
+
+	/**
+	 * 注册 
+	 * @param username 用户名
+	 * @param password 密码
+	 * @param telphone 电话
+	 * @param state 状态值
+	 * @param usertype 用户类别
+	 * @return
+	 */
+	int register(@Param("username")String username,@Param("password") String password,@Param("telphone") String telphone, int state, int usertype);
+
+}
