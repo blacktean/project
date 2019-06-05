5c6ddb77ead0fa9c8c5545e2c4a2c483d7d4ffb1
lishan <1518219928@qq.com>
2019-06-05 16:23:12 +0800

    单笔汇款页面 by hjh

--- bankTransfer/bankTransfer-front/src/main/java/com/bankTransfer/web/PageController.java
+++ bankTransfer/bankTransfer-front/src/main/java/com/bankTransfer/web/PageController.java
@@ -12,6 +12,7 @@ import com.bankTransfer.pojo.CardType;
 import com.bankTransfer.pojo.Currency;
 import com.bankTransfer.pojo.Document;
 import com.bankTransfer.service.IBaseService;
+import com.bankTransfer.util.RequireLogin;
 import com.bankTransfer.util.UserContext;
 
 
@@ -21,7 +22,7 @@ import com.bankTransfer.util.UserContext;
  *
  */
 @Controller
-public class PageController {
+public class PageController { 
 	@Autowired
 	private IBaseService baseService;
 	
@@ -29,7 +30,7 @@ public class PageController {
 	public String toBatchTransfer(Model model) {
 		List<Card> cards = baseService.queryCardByUserId(UserContext.getCurrent().getId());
 		model.addAttribute("cards", cards);
-		return "forward:/batchTransfer.html";
+		return "batchTransfer";
 	}
 	@GetMapping("toCrossBorderTransfer")
 	public String toCrossBorderTransfer(Model model) {
@@ -41,25 +42,26 @@ public class PageController {
 		model.addAttribute("cards", cards);
 		model.addAttribute("cardTypes", cardTypes);
 		model.addAttribute("documents", documents);
-		return "forward:/crossBorderTransfer.html";
-	}
-	
+		return "crossBorderTransfer";
+	} 
+	 
 	@GetMapping("toRegisterAccountTransfer")
 	public String toRegisterAccountTransfer(Model model) {
 		List<Card> cards = baseService.queryCardByUserId(UserContext.getCurrent().getId());
 		List<Document> documents = baseService.queryDocument();
 		model.addAttribute("cards", cards);
 		model.addAttribute("documents", documents);
-		return "forward:/registerAccountTransfer.html";
+		return "registerAccountTransfer";
 	}
 	
 	@GetMapping("toSingleTransfer")
+	@RequireLogin
 	public String toSingleTransfer(Model model) {
 		List<Card> cards = baseService.queryCardByUserId(UserContext.getCurrent().getId());
 		List<CardType> cardTypes = baseService.queryCardType();
 		model.addAttribute("cardTypes", cardTypes);
 		model.addAttribute("cards", cards);
-		return "forward:/singleTransfer.html";
+		return "singleTransfer";
 	}
 	
 }
