32b5ecb1a0e80a1f20adfb6b06936fdf3207ceaf
fangjia <1054914436@qq.com>
2019-06-05 16:24:46 +0800

    xgf

--- bankTransfer/bankTransfer-core/src/main/java/com/bankTransfer/service/ITransferService.java
+++ bankTransfer/bankTransfer-core/src/main/java/com/bankTransfer/service/ITransferService.java
@@ -1,5 +1,18 @@
 package com.bankTransfer.service;
 
+import java.math.BigDecimal;
+
+import com.bankTransfer.pojo.TransferSingle_VO;
+
 public interface ITransferService {
+	/**减少指定账户上的指定金额*/
+	void subtractBalance(BigDecimal transferAmount,String paymentAccount);
+	/**判断指定账户是否为当前用户的常用联系账户*/
+	boolean judgeContact(String receiveCardId);
+	/**给当前用户添加一个常用联系人*/
+	void addContact(String receiveName, String receiveCardId);
+	/**插入一条转账交易成功记录*/
+	void insertRecord(TransferSingle_VO singleVO);
+	
 
 }
--- bankTransfer/bankTransfer-core/src/main/java/com/bankTransfer/service/impl/TransferServiceImpl.java
+++ bankTransfer/bankTransfer-core/src/main/java/com/bankTransfer/service/impl/TransferServiceImpl.java
@@ -1,9 +1,13 @@
 package com.bankTransfer.service.impl;
 
+import java.math.BigDecimal;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.bankTransfer.mapper.TransferMapper;
+import com.bankTransfer.pojo.TransferSingle_VO;
 import com.bankTransfer.service.ITransferService;
 
 /*
@@ -15,7 +19,27 @@ public class TransferServiceImpl implements ITransferService {
 
 	@Autowired
 	private TransferMapper transferMapper;
-	
-	
+
+	@Override
+	@Transactional
+	public void subtractBalance(BigDecimal transferAmount, String paymentAccount) {
+		transferMapper.subtractBalance(transferAmount, paymentAccount);		
+	}
+
+	@Override
+	public boolean judgeContact(String receiveCardId) {
+		return transferMapper.judgeContact(receiveCardId);
+	}
+
+	@Override
+	public void addContact(String receiveName, String receiveCardId) {
+		transferMapper.addContact(receiveName, receiveCardId);
+	}
+
+	@Override
+	public void insertRecord(TransferSingle_VO singleVO) {
+		transferMapper.insertRecord(singleVO);
+	}
+
 	
 }
