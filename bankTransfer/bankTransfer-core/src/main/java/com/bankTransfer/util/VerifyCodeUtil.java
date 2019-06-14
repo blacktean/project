aed02c42a7697e86417b29ce0649b16ccb68f3ca
fangjia <1054914436@qq.com>
2019-06-14 12:41:59 +0800

    --fj

--- bankTransfer/bankTransfer-core/src/main/java/com/bankTransfer/util/VerifyCodeUtil.java
+++ bankTransfer/bankTransfer-core/src/main/java/com/bankTransfer/util/VerifyCodeUtil.java
@@ -1,5 +1,6 @@
 package com.bankTransfer.util;
 
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -8,11 +9,20 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import org.apache.http.HttpEntity;
 import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
 
+import com.alibaba.fastjson.JSONObject;
 import com.bankTransfer.pojo.VerifyCodeVo;
+import com.sun.org.apache.xerces.internal.impl.xpath.regex.ParseException;
 
 /**
  * 发送验证码的工具类
@@ -20,7 +30,7 @@ import com.bankTransfer.pojo.VerifyCodeVo;
  *
  */
 
-public class VerifyCodeUtil {
+/*public class VerifyCodeUtil {
 	private static int[] seeds = {0,1,2,3,4,5,6,7,8,9};
 	public static void sendMsg(String phoneNumber) {
 		String code = randomCode(6);
@@ -33,81 +43,81 @@ public class VerifyCodeUtil {
 		System.out.println(code);
 		UserContext.setCurrentVerifyCodeVo(vo);
 		System.out.println(UserContext.getCurrentVerifyCodeVo());;
+	}*/
+
+
+
+
+
+
+public class VerifyCodeUtil {
+	private static int[] seeds = {0,1,2,3,4,5,6,7,8,9};
+	public static void sendMsg(String phoneNumber) {
+		CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+		String code = randomCode(6);
+		URI uri=null;
+		List<NameValuePair> params = new ArrayList<>();
+		params.add(new BasicNameValuePair("mobile", phoneNumber));
+		params.add(new BasicNameValuePair("tpl_id", "157441"));
+		params.add(new BasicNameValuePair("tpl_value", encode(code)));
+		params.add(new BasicNameValuePair("key", "95856558f2b9be44130f422a2dd3636a"));
+		try {
+			uri=new URIBuilder().setPath("http://v.juhe.cn/sms/send").setParameters(params).build();
+		} catch (URISyntaxException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		HttpPost httpPost = new HttpPost(uri);
+		CloseableHttpResponse response = null;
+		try {
+			response=httpClient.execute(httpPost);
+		} catch (ClientProtocolException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		HttpEntity httpEntity = response.getEntity();
+		
+		try {
+			String entity = EntityUtils.toString(httpEntity, "UTF-8");
+			if(httpEntity!=null) {
+				if(response.getStatusLine().getStatusCode()==200) {
+					VerifyCodeVo vo = JSONObject.parseObject(entity,VerifyCodeVo.class );
+					if(vo.getError_code()==0) {
+						vo.setPhoneNumber(phoneNumber);
+						vo.setLastTime(new Date());
+						vo.setVerifyCode(code);
+						UserContext.setCurrentVerifyCodeVo(vo);
+					}
+					else {
+						throw new RuntimeException("发送失败");
+					}
+				}
+			}
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			try {
+				// 释放资源
+				if (httpClient != null) {
+					httpClient.close();
+				}
+				if (response != null) {
+					response.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+
 	}
-
-
-
-
-
-
-//public class VerifyCodeUtil {
-//	private static int[] seeds = {0,1,2,3,4,5,6,7,8,9};
-//	public static void sendMsg(String phoneNumber) {
-//		CloseableHttpClient httpClient = HttpClientBuilder.create().build();
-//		String code = randomCode(6);
-//		URI uri=null;
-//		List<NameValuePair> params = new ArrayList<>();
-//		params.add(new BasicNameValuePair("mobile", phoneNumber));
-//		params.add(new BasicNameValuePair("tpl_id", "157441"));
-//		params.add(new BasicNameValuePair("tpl_value", encode(code)));
-//		params.add(new BasicNameValuePair("key", "95856558f2b9be44130f422a2dd3636a"));
-//		try {
-//			uri=new URIBuilder().setPath("http://v.juhe.cn/sms/send").setParameters(params).build();
-//		} catch (URISyntaxException e1) {
-//			// TODO Auto-generated catch block
-//			e1.printStackTrace();
-//		}
-//		HttpPost httpPost = new HttpPost(uri);
-//		CloseableHttpResponse response = null;
-//		try {
-//			response=httpClient.execute(httpPost);
-//		} catch (ClientProtocolException e1) {
-//			// TODO Auto-generated catch block
-//			e1.printStackTrace();
-//		} catch (IOException e1) {
-//			// TODO Auto-generated catch block
-//			e1.printStackTrace();
-//		}
-//		HttpEntity httpEntity = response.getEntity();
-//		
-//		try {
-//			String entity = EntityUtils.toString(httpEntity, "UTF-8");
-//			if(httpEntity!=null) {
-//				if(response.getStatusLine().getStatusCode()==200) {
-//					VerifyCodeVo vo = JSONObject.parseObject(entity,VerifyCodeVo.class );
-//					if(vo.getError_code()==0) {
-//						vo.setPhoneNumber(phoneNumber);
-//						vo.setLastTime(new Date());
-//						vo.setVerifyCode(code);
-//						UserContext.setCurrentVerifyCodeVo(vo);
-//					}
-//					else {
-//						throw new RuntimeException("发送失败");
-//					}
-//				}
-//			}
-//		} catch (ParseException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}finally {
-//			try {
-//				// 释放资源
-//				if (httpClient != null) {
-//					httpClient.close();
-//				}
-//				if (response != null) {
-//					response.close();
-//				}
-//			} catch (IOException e) {
-//				e.printStackTrace();
-//			}
-//		}
-//		
-//
-//	}
 	/**
 	 * 随机生成验证码
 	 * @param length  验证码的长度
