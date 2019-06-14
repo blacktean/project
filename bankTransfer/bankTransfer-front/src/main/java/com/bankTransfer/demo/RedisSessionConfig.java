a17de9ebf4ac1a6909d4ee7280c6372e85dbb04d
hcf <1429187588@qq.com>
2019-06-14 13:55:26 +0800

    redis

--- bankTransfer/bankTransfer-front/src/main/java/com/bankTransfer/demo/RedisSessionConfig.java
+++ bankTransfer/bankTransfer-front/src/main/java/com/bankTransfer/demo/RedisSessionConfig.java
@@ -1,23 +1,23 @@
-//package com.bankTransfer.demo;
-//
-//import org.springframework.cache.CacheManager;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.data.redis.cache.RedisCacheManager;
-//import org.springframework.data.redis.connection.RedisConnectionFactory;
-//import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
-//
-//@Configuration
-//
-//@EnableRedisHttpSession  (maxInactiveIntervalInSeconds = 1800)
-//public class RedisSessionConfig {
-//
-//	
-//	
-//	 @Bean
-//	public CacheManager cacheManager(RedisConnectionFactory factory) {
-//	 RedisCacheManager cacheManager = RedisCacheManager.create(factory);
-//	 	return cacheManager;	
-//	 }
-//	
-//}
+package com.bankTransfer.demo;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+@Configuration
+
+@EnableRedisHttpSession  (maxInactiveIntervalInSeconds = 1800)
+public class RedisSessionConfig {
+
+	
+	
+	 @Bean
+	public CacheManager cacheManager(RedisConnectionFactory factory) {
+	 RedisCacheManager cacheManager = RedisCacheManager.create(factory);
+	 	return cacheManager;	
+	 }
+	
+}
